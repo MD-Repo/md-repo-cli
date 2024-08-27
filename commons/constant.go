90d2d560c029a91ed20e927e54ba30003132221c
Illyoung Choi <iychoi@email.arizona.edu>
2024-08-27 13:15:30 -0700

    Fix typo

--- commons/constant.go
+++ commons/constant.go
@@ -10,7 +10,7 @@ const (
 	filesystemTimeout time.Duration = 10 * time.Minute
 
 	TransferTreadNumDefault    int    = 5
-	UploadTreadNumMax          int    = 20
+	UploadThreadNumMax         int    = 20
 	TcpBufferSizeDefault       int    = 4 * 1024 * 1024
 	TcpBufferSizeStringDefault string = "4MB"
 
